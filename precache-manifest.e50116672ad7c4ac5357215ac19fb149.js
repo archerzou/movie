6a3d221e25904960a92ac639cdab00bb92473a1d
Zou <kevin.zou@honeywell.com>
2020-06-11 18:00:38 +0800

    Updates

---
+++ precache-manifest.e50116672ad7c4ac5357215ac19fb149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c510b0f709769ed3db479fc63186b3e",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "28ffcf3097dbddd79b40",
+    "url": "/movie/static/css/main.e825602e.chunk.css"
+  },
+  {
+    "revision": "63af2916ba184a0ba216",
+    "url": "/movie/static/js/2.c18d28f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.c18d28f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ffcf3097dbddd79b40",
+    "url": "/movie/static/js/main.c5be3d26.chunk.js"
+  },
+  {
+    "revision": "e9c4e6fbaaf66c4b47e7",
+    "url": "/movie/static/js/runtime-main.3ee56faf.js"
+  }
+]);
